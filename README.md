529c9398e063b3d9b6f5266e66bf699f3b4487ac
Ramon Fontes <ramonreisfontes@gmail.com>
2015-08-13 14:15:25 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+###Instructions:  
+
+You have to set up some important system variables. To do that please run the following commands:  
+
+`ulimit -n 65000`    
+`echo "kernel.pty.max = 50000" >> /etc/sysctl.conf`    
+`sysctl -p`  
+
+###Running the code:  
+
+To run the code just type `python mininetPTT.py`, but you have to choose the PTT (IXP in Portuguese) before running it (assigning a name to 'filename' inside of the mininetPTT.py file).  
---
+++ mininetPTT.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+"""
+Mininet Code to run PTT (Pontos de troga de tráfego no Mininet) or IXP (Internet Exchange Points)
+author: Ramon Fontes - ramonrf@dca.fee.unicamp.br / 
+                       ramonreisfontes@gmail.com
+"""
+
+import networkx as nx
+import os
+
+from mininet.net import Mininet
+from mininet.node import Controller, OVSKernelSwitch, UserSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.link import Link, TCLink
+
+
+def topology():
+    
+    #PTT name
+    filename='Ptt_Path_BA.txt'
+
+    "Create a network."
+    net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )
+
+    i=0;k=1;j=0
+    node = [[0 for x in xrange(50)] for x in xrange(333300)]
+    e1=open(str(filename), 'r') # read info file
+    A=nx.Graph()
+
+    for line in e1:
+        count=len(line.strip().split(' '))
+        while i <= count:
+            if (i==0):
+                node[j][i]=26121
+            elif((i>0) and (node[j][k-1]!=int(line.strip().split(' ')[i-1]))):
+                node[j][k] = int(line.strip().split(' ')[i-1])
+                A.add_edge(node[j][k-1],node[j][k])
+	        k+=1
+            i+=1
+        i=0
+        k=1
+        j+=1
+
+    print "*** Creating %s nodes"%len(A.nodes())
+    asn=[0 for x in xrange(len(A.nodes()))]
+    x=0
+
+    for nodes in A.nodes():
+        asn[x] = net.addSwitch( 'ASN%s' % nodes )
+        x+=1
+
+    #h2 = net.addHost( 'h2', mac='00:00:00:00:00:02', ip='10.0.0.2/8' )
+    c3 = net.addController( 'c3', controller=RemoteController, ip='127.0.0.1', port=6633 )
+
+    print "*** Creating links"
+    i=0;k=1;j=0
+    node = [[0 for x in xrange(50)] for x in xrange(333300)]
+    e2=open(str(filename), 'r') # read info file
+
+    for line in e2:
+        count=len(line.strip().split(' '))
+        while i <= count:
+            if (i==0):
+                node[j][i]=26121
+            elif((i>0) and (node[j][k-1]!=int(line.strip().split(' ')[i-1]))):
+                node[j][k] = int(line.strip().split(' ')[i-1])
+                #print net.addLink("ASN%s"%node[j][k-1],"ASN%s"%node[j][k])
+                k+=1
+            i+=1
+        i=0
+        k=1
+        j+=1
+
+    print "*** Starting network"
+    net.build()
+    c3.start()
+
+    x=0
+    for nodes in A.nodes():
+        asn[x].start( [c3] )
+        x+=1
+
+    print "*** Running CLI"
+    CLI( net )
+
+    print "*** Stopping network"
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    topology()
+

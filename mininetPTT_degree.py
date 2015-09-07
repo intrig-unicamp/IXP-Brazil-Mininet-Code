bb70438240c07a7481b0db39e8d94657bf244c9c
Ramon Fontes <ramonreisfontes@gmail.com>
2015-09-07 13:21:24 -0300

    Update mininetPTT_degree.py

--- mininetPTT_degree.py
+++ mininetPTT_degree.py
@@ -7,6 +7,7 @@ author: Ramon Fontes - ramonrf@dca.fee.unicamp.br
 """
 
 import networkx as nx
+import os
 
 from mininet.net import Mininet
 from mininet.node import Controller, OVSKernelSwitch
@@ -14,15 +15,30 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 from mininet.link import TCLink
 
+outputFileName = 'mininetCode.py'
+
 def topology():
+    
+    if(os.path.exists('mininetCode.py')):
+        os.system('rm mininetCode.py')
+    
+    os.system('echo "from mininet.net import Mininet" >> %s' % outputFileName)
+    os.system('echo "from mininet.node import Controller, OVSKernelSwitch" >> %s' % outputFileName)
+    os.system('echo "from mininet.cli import CLI" >> %s' % outputFileName)
+    os.system('echo "from mininet.log import setLogLevel" >> %s' % outputFileName)
+    os.system('echo "from mininet.link import TCLink" >> %s' % outputFileName)
+    
+    os.system('echo "\ndef topology():" >> %s' % outputFileName)
     
     removeNodes = []
     hashost = []
+   
     filename = raw_input('Please enter the filename: ')
 
     "Create a network."
     net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )
-
+    os.system('echo "\n    net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )" >> %s\n\n' % (outputFileName))
+    
     excludedLine = 0
     i=0;k=1;j=0
     node = [[0 for x in xrange(50)] for x in xrange(333300)]
@@ -59,20 +75,26 @@ def topology():
     print "*** Creating %s nodes"%len(A.nodes())
     asn=[0 for x in xrange(len(A.nodes()))]
     x=0
-
-    for node in A.nodes():
-        asn[x] = net.addSwitch( 'ASN%s' % node )
+    os.system('echo "\n    print \'*** Creating %s nodes ***\'" >> %s' % (len(A.nodes()), outputFileName))
+    
+    for switch in A.nodes():
+        asn[x] = net.addSwitch( 'ASN%s' % switch )
+        os.system('echo "    ASN%s = net.addSwitch( \'ASN%s\' )" >> %s' % (switch, switch, outputFileName)) 
         x+=1
     
     for host in hosts:
         for node in A.nodes():
             if str(host) == str(node):
                 net.addHost('h%s' % str(host))
+                print "net.addHost( 'h%s' )" % str(host)
+                os.system('echo "    h%s = net.addHost( \'h%s\' )" >> %s' % (str(host), str(host), outputFileName)) 
                 A.add_edge(str(node), str(host))    
     
-    c3 = net.addController( 'c3' )
+    c1 = net.addController( 'c1' )
+    os.system('echo "\n    c1 = net.addController( \'c1\' )" >> %s' % (outputFileName))
 
-    print "*** Creating links"
+    print "*** Creating links ***"
+    os.system('echo "\n    print \'*** Creating Links ***\'" >> %s' % (outputFileName))
     i=0;k=1;j=0
     node = [[0 for x in xrange(50)] for x in xrange(333300)]
     e2=open(str(filename), 'r') # read info file
@@ -97,36 +119,56 @@ def topology():
                         if str(node[j][k]) in hosts and node[j][k] not in hashost:
                             print net.addLink("ASN%s"%node[j][k],'h%s'%node[j][k])
                             print net.addLink("ASN%s"%node[j][k-1],"ASN%s"%node[j][k], bw=bw, delay=delay)
+                            os.system('echo "    net.addLink(\'ASN%s\',\'h%s\')" >> %s' % (node[j][k], node[j][k], outputFileName)) 
+                            os.system('echo "    net.addLink(\'ASN%s\',\'ASN%s\', bw=%s, delay=\'%s\')" >> %s' % (node[j][k-1], node[j][k], bw, delay, outputFileName)) 
                             hashost.append(node[j][k])
                         elif str(node[j][k-1]) in hosts and node[j][k-1] not in hashost:
                             print net.addLink('h%s'%node[j][k],"ASN%s"%node[j][k])
                             print net.addLink("ASN%s"%node[j][k-1],"ASN%s"%node[j][k], bw=bw, delay=delay)
+                            os.system('echo "    net.addLink(\'h%s\',\'ASN%s\'%)" >> %s' % (node[j][k], node[j][k], outputFileName)) 
+                            os.system('echo "    net.addLink(\'ASN%s\',\'ASN%s\', bw=%s, delay=\'%s\')" >> %s' % (node[j][k-1], node[j][k], bw, delay, outputFileName)) 
                             hashost.append(node[j][k-1])
                         if str(node[j][k]) not in hosts and str(node[j][k-1]) not in hosts:
                             print net.addLink("ASN%s"%node[j][k-1],"ASN%s"%node[j][k], bw=bw, delay=delay)
+                            os.system('echo "    net.addLink(\'ASN%s\',\'ASN%s\', bw=%s, delay=\'%s\')" >> %s' % (node[j][k-1], node[j][k], bw, delay, outputFileName)) 
                         k+=1
                 i+=1
             i=0
             k=1
             j+=1
 
-    print "*** Starting network"
+    print "*** Starting network ***"
+    os.system('echo "\n    print \'*** Starting network ***\'" >> %s' % (outputFileName)) 
+    
     net.build()
-    c3.start()
-
+    c1.start()
+    os.system('echo "    net.build()" >> %s' % (outputFileName)) 
+    os.system('echo "    c1.start()\n" >> %s' % (outputFileName)) 
+    
     x=0
     for nodes in A.nodes():
         if nodes not in hosts:
-            asn[x].start( [c3] )
+            asn[x].start( [c1] )
+            os.system('echo "    %s.start( [c1] )" >> %s' % (asn[x], outputFileName)) 
             x+=1
 
     print "*** Running CLI"
     CLI( net )
+    os.system('echo "\n    print \'*** Running CLI ***\'" >> %s' % (outputFileName)) 
+    os.system('echo "    CLI( net )" >> %s' % (outputFileName)) 
+   
 
     print "*** Stopping network"
     net.stop()
+    os.system('echo "\n    print \'*** Stopping network ***\'" >> %s' % (outputFileName)) 
+    os.system('echo "    net.stop()" >> %s' % (outputFileName)) 
+   
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
     topology()
+    
+    os.system('echo "\nif __name__ == \'__main__\':" >> %s' % (outputFileName)) 
+    os.system('echo "    setLogLevel( \'info\' )" >> %s' % (outputFileName)) 
+    os.system('echo "    topology()" >> %s' % (outputFileName)) 
 
